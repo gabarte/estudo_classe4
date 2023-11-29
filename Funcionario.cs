95d5d1c24cbc9dc55a46e587846a72055e0fa5a0
gabarte <129518419+gabarte@users.noreply.github.com>
2023-11-29 17:54:23 -0300

    Add files via upload

---
+++ Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2_classe_funcionário
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome
+            + ", R$"
+            + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using ex2_classe_funcionário;
+using System.Globalization;
+
+Funcionario f = new Funcionario();
+
+Console.WriteLine("Entre com os dados do funcionário:");
+Console.Write("Nome: ");
+f.Nome = Console.ReadLine();
+
+Console.WriteLine();
+Console.Write("Salário bruto: ");
+f.SalarioBruto = double.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.Write("Imposto: ");
+f.Imposto = double.Parse(Console.ReadLine());
+
+Console.WriteLine();
+double salarioLiquido = f.SalarioLiquido();
+Console.Write("Salário Liquido: " + salarioLiquido, CultureInfo.InvariantCulture);
+
+Console.WriteLine();
+Console.WriteLine("Digite a porcentagem para aumentar o salário: ");
+double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+f.AumentarSalario(porcent);
+
+Console.WriteLine();
+Console.Write("Dados do funcionário: " + f);
